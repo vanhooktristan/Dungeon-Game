36be519f4fe555b954a238af834110a159f6bd49
Melissa Waltner <melissamwaltner@gmail.com>
2018-05-10 18:50:54 -0500

    Fixed models/index.js file, added test code to index.pug, put basic structure into server.js, put in a test route to render pug pages, added a new package.

---
+++ routes/html-routes.js
@@ -0,0 +1,9 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended:  true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+app.set("views", "views");
+app.use(express.static("public"));
+
+//Routes
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({ force: true}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on localhost:" + PORT);
+  });
+});
