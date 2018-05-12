45932b06408a01c5aa3f23b96e71d3b5a2697b52
Jeff Karr <jkarr001@gmail.com>
2018-05-12 10:53:26 -0500

    opening desktop

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,10 @@
 var path = require("path");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+  app.get("/", (req, res) => {
+    //  res.render("index");
+    res.render("index2");
+  });
 
 };
