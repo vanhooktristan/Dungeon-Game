6f9710b1130162c889210174851723781b8f733f
Jeff Karr <jkarr001@gmail.com>
2018-05-12 11:21:17 -0500

    test

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,15 @@ var path = require("path");
 
 module.exports = function (app) {
 
+<<<<<<< HEAD
   app.get("/", (req, res) => {
     //  res.render("index");
     res.render("index2");
   });
+=======
+app.get("/", (req, res) => {
+  res.render("index", {title: "Dungeon Game | Start"});
+});
+>>>>>>> f7d7c43d173ca94241c8c3bd8d76c890d704ca30
 
 };
