0e236975f59cbf0e98470100dd40cba6777c8ed9
Melissa Waltner <melissamwaltner@gmail.com>
2018-05-17 14:19:53 -0500

    Battle page works and will get latest character on creation.

--- public/assets/js/battle.js
+++ public/assets/js/battle.js
@@ -7,7 +7,7 @@ $(function () {
     var j = 0;
     var player1 = new Character(data[i].name, data[i].strength, data[i].vitality, data[i].agility);
     var monster1 = new Character(data[j].name, data[j].strength, data[j].vitality, data[j].agility);
-  
+
     $("#round_num").text("Round " + count);
     $("#health_player").text("Player: " + player1.hp);
     $("#health_monster").text("Monster: " + monster1.hp);
@@ -17,58 +17,57 @@ $(function () {
 
     $("#attack").on("click", function (event) {
       event.preventDefault();
+      if (player1.hp >= 0 && monster1.hp >= 0) {
       count += 1;
-        $("#round_num").text("Round " + count);
-        $("#round_info").text("");
-        player1.battle(monster1);
-        
-        $("#health_player").text("Player: " + player1.hp);
-        $("#health_monster").text("Monster: " + monster1.hp);
+      $("#round_num").text("Round " + count);
+      $("#round_info").text("");
+      player1.battle(monster1);
+
+      $("#health_player").text("Health: " + player1.hp);
+      $("#health_monster").text("Health: " + monster1.hp);
+    }
     });
   });
-  
 
-
-function Character(name, str, vit, agi) {
-  this.name = name;
-  this.str = str;
-  this.vit = vit;
-  this.agi = agi;
-  this.hp = 10 + (vit*2);
-  this.def = Math.round(str/3 + vit);
-  this.spd = Math.ceil(3 + agi*1.5);
-}
-
-Character.prototype.battle =  function(opponent) {
-  
-
-  function round(a, d) {
-    var damage = Math.ceil(((a.str/2) * 2.5) - d.def/2);
-    var chance = Math.ceil((a.spd/d.spd) * 20 + ((Math.random()*100)+ 50));
-
-    console.log(a.name + ": " + a.hp + " || " + d.name + ": " + d.hp);
-    
-
-    if (chance >= 100) {
-      d.hp -= damage;
-      $("#round_info").append("<p>" + d.name + " took " + damage + " damage!</p>");
-      console.log(d.name + " took " + damage + " damage!");
-    } else {
-      $("#round_info").append("<p>" + a.name + " missed the attack</p>");
-      console.log(a.name + " missed the attack");
-    }
-    console.log(a.name + ": " + a.hp + " || " + d.name + ": " + d.hp);
-    console.log("Damage: " + damage, "Chance: " + chance);
-    console.log(".........");
+  function Character(name, str, vit, agi) {
+    this.name = name;
+    this.str = str;
+    this.vit = vit;
+    this.agi = agi;
+    this.hp = 10 + (vit * 2);
+    this.def = Math.round(str / 3 + vit);
+    this.spd = Math.ceil(3 + agi * 1.5);
   }
 
-  round(this, opponent);
-  round(opponent, this);
-}
+  Character.prototype.battle = function (opponent) {
+    function round(a, d) {
+      var damage = Math.ceil(((a.str / 2) * 2.5) - d.def / 2);
+      var chance = Math.ceil((a.spd / d.spd) * 20 + ((Math.random() * 100) + 50));
 
-Character.prototype.printStats = function() {
-  console.log(this.name, this.str, this.vit, this.agi, this.hp, this.def, this.spd);
-}
+    // if (d.hp >= 0 && a.hp >= 0) {
+      if (chance >= 100) {
+        d.hp -= damage;
+        $("#round_info").append("<p>" + d.name + " took " + damage + " damage!</p>");
+        console.log(d.name + " took " + damage + " damage!");
+      } else {
+        $("#round_info").append("<p>" + a.name + " missed the attack</p>");
+        console.log(a.name + " missed the attack");
+      }
+    // } else {
+
+    // }
+    } //end of round function
+
+    round(this, opponent);
+    round(opponent, this);
+    
+  } //end of battle function
+
+  Character.prototype.printStats = function () {
+    console.log(this.name, this.str, this.vit, this.agi, this.hp, this.def, this.spd);
+  }
 
 
 });
+
+//need to remove unneed console.logs
--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,3 @@
 USE game_db;
 
-INSERT INTO monsters (`name`, strength, vitality, agility, `image`) VALUES ("Wolf", 4, 2, 3, "#.png");
+INSERT INTO monsters (`name`, strength, vitality, agility, `image`) VALUES ("Wolf", 4, 2, 3, "monster-1-pic.png");
