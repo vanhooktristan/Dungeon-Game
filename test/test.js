a446ceb47b84533491087b9afc25ad99eac2409d
vanhooktristan <vanhooktristan@gmail.com>
2018-05-19 13:35:03 -0500

    adding nightmare testing

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe("Monster Slayer", function() {
   // Extending it to 30 seconds to have time to load the pages
 
   this.timeout(30000);
-  it("should open the game's first page", function(done) {
+  it("should open the game's first page and click the start button", function(done) {
     // ID for the login button.
     Nightmare({ show: true })
       .goto("https://monster-slayer-ku.herokuapp.com/")
@@ -26,8 +26,21 @@ describe("Monster Slayer", function() {
       });
   });
 
-  it("should throw an error for fun", function() {
-    throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
-    done();
-  });
+  it("should enter a name into the name form", function(done) {
+    Nightmare({ show: true })
+        .goto("https://monster-slayer-ku.herokuapp.com/character")
+        .type("#name-input", "Test Slayer")
+        .select("#strength-input", "3")
+        .wait(3000)
+        // Evaluate the title
+        .evaluate(function() {
+            return document.title;
+        })
+        .end()
+        // Asset the title is as expected
+        .then(function(title) {
+            expect(title).to.equal("Monster Slayer");
+            done()
+        });
+    });
 });
